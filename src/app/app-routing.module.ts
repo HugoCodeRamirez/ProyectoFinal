a9e01126e35d7169e7083a6b2a7da8c1d4346bc7
HugoCodeRamirez <hugo-ramirez_@hotmaill.com>
2022-06-13 19:38:24 -0500

    ProyectoFinal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CarritoComprasComponent } from './components/carrito-compras/carrito-compras.component';
+import { CatalogoComponent } from './components/catalogo/catalogo.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+{path: '', component: HomeComponent},
+{path: 'carro-de-compras', component: CarritoComprasComponent},
+{path: 'catalogo', component: CatalogoComponent},
+{path: 'login', component: LoginComponent},
+{path: '404', component: NotFoundComponent},
+
+{path: '**', redirectTo: '404', pathMatch: 'full'}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
+import { CarritoComprasComponent } from './components/carrito-compras/carrito-compras.component';
+import { CatalogoComponent } from './components/catalogo/catalogo.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    LoginComponent,
+    CarritoComprasComponent,
+    CatalogoComponent,
+    DashboardComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
