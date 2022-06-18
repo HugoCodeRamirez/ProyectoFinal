d9795623f88c95837aa506b4f423eb10b6968758
RicardoMend02 <100816611+RicardoMend02@users.noreply.github.com>
2022-06-17 20:58:39 -0500

    cambios ricardo

--- express/index.js
+++ express/index.js
@@ -1,1 +1,7 @@
-const express=require('express');
+
+
+
+
+
+
+app.use('/api/contacto',require('./routes/contactoRutas'))
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -11,13 +11,14 @@ export class LoginComponent implements OnInit {
     usuarioForm: FormGroup;
     reyexCorreo = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i
     reyexNumero = /^[0-9]+$/;
+    regexLetra = /^[a-zA-Z\s]+$/;
 
     constructor(private fb: FormBuilder) {
         this.usuarioForm = this.fb.group({
             correo: ['', [Validators.required, Validators.pattern(this.reyexCorreo)]],
-            nombres: ['',[Validators.required]],
-            apellidos: ['',[Validators.required]],
-            contraseña: ['',Validators.required],
+            nombres: ['',[Validators.required, Validators.pattern(this.regexLetra)]],
+            apellidos: ['',[Validators.required, Validators.pattern(this.regexLetra)]],
+            contrasena: ['',Validators.required],
             edad: ['',[Validators.required, Validators.pattern(this.reyexNumero)]]
 
         })
@@ -31,7 +32,7 @@ export class LoginComponent implements OnInit {
             correo: this.usuarioForm.get('correo')?.value,
             nombres: this.usuarioForm.get('nombres')?.value,
             apellidos: this.usuarioForm.get('apellidos')?.value,
-            contraseña: this.usuarioForm.get('contraseña')?.value,
+            contrasena: this.usuarioForm.get('contrasena')?.value,
             edad: this.usuarioForm.get('edad')?.value
         }
         console.log(contactoUsuario)
