cb0518b25df7cff56b5f9189df9f6201c58952d3
andresg9805 <andresg35@outlook.es>
2022-06-20 19:28:56 -0500

    cambios

--- express/index.js
+++ express/index.js
@@ -2,9 +2,10 @@ const express=require('express');
 const conectarDB=require('./config/db')
 const app=express();
 
+conectarDB();
+app.use(express.json());
 
-
-
-
-
-app.use('/api/contacto',require('./routes/contactoRutas'))
+app.use('/api/productos',require('./routes/producto'))
+app.listen(4000,()=>{
+    console.log('servidor en la parte superior')
+})
