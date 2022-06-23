325efaf331ca3031935043dabd369b96df9aafd0
andresg9805 <andresg35@outlook.es>
2022-06-23 15:57:54 -0500

    cambios

--- src/app/components/computadores/computadores.component.ts
+++ src/app/components/computadores/computadores.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup,Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-computadores',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComputadoresComponent implements OnInit {
 
-  constructor() { }
+  computadorForm:FormGroup;
+  regexCorreo = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i
+  regexNumero = /^[0-9]+$/;
+  regexLetra = /^[a-zA-Z\s]+$/;
+  id:string|null;
+  constructor(private fb:FormBuilder,private router:Router,private idRoute:ActivatedRoute) {
+    this.computadorForm=this.fb.group({
+      marca:['',[Validators.required,Validators.pattern(this.regexLetra)]],
+      memoriaram:['',[Validators.required],Validators.pattern(this.regexLetra),Validators.pattern(this.regexNumero)],
+      discosolido:['',[Validators.required,Validators.pattern(this.regexLetra),Validators.maxLength(2)]],
+      precio:['',[Validators.required,Validators.pattern(this.regexNumero),Validators.minLength(5)]]
+    })
+    this.id=this.idRoute.snapshot.paramMap.get('id')
+   }
 
   ngOnInit(): void {
   }
 
+  agregarComputador(){
+    console.log(this.computadorForm)
+  }
 }
--- src/app/components/consolas/consolas.component.ts
+++ src/app/components/consolas/consolas.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup,Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-consolas',
@@ -7,7 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConsolasComponent implements OnInit {
 
-  constructor() { }
+  consolaForm:FormGroup;
+  regexCorreo = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i
+  regexNumero = /^[0-9]+$/;
+  regexLetra = /^[a-zA-Z\s]+$/;
+  id:string|null;
+  constructor(private fb:FormBuilder,private router:Router,private idRoute:ActivatedRoute) {
+    this.consolaForm=this.fb.group({
+      marca:['',[Validators.required,Validators.pattern(this.regexLetra)]],
+      tipodecontroles:['',[Validators.required]],
+      controlesincluidos:['',[Validators.required,Validators.pattern(this.regexLetra),Validators.maxLength(2)]],
+      precio:['',[Validators.required,Validators.pattern(this.regexNumero),Validators.minLength(5)]]
+    })
+    this.id=this.idRoute.snapshot.paramMap.get('id')
+   }
 
   ngOnInit(): void {
   }
